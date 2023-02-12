a247ad6465762bddcb6a91b8c1a63d3e3761b515
laksman <dielaksman98@gmail.com>
2023-02-12 18:41:41 +0700

    install ejs

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
-  console.log("Time:", Date.now());
-  next();
-});
 // home
-app.get("/", (request, response) => response.render("index"));
 
 // game
 app.get("/game", (request, response) => response.render("game"));
